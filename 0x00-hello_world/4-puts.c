0a7e9ad0d815b4997d60851ecd1c27c7503554ac
Palezeze <palesashole@yahoo.com>
2023-06-15 19:28:35 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 /**
- * main -prints the phase 
- * Return: 0 if excited prperly, non-zero otherwise
+ * main -Entry point
+ *
+ * Return: 0 (Sucessful)
  */
 int main(void)
 {
-	put("\"Programming is like building a multilingual puzzle");
-	return (o);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
