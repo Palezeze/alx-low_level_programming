866ce48b442dc8e30208447af0477c5490d7d169
Palezeze <palesashole@yahoo.com>
2023-06-18 14:38:29 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main -prints the phrase 
+ *
+ * Return: 1 (if successful)
+ */
+int main(void)
+{
+	printf("and that piece of art is useful" (- Dora Korpar,\'n)2015-10-19");
+	return(1);
+}
