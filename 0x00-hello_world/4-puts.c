9195ca1808df20fdcf85580c095018cb77d1c8e9
Palezeze <palesashole@yahoo.com>
2023-06-18 12:11:41 -0700

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 /**
- * main -Entry point
- *
- * Return: 0 (Sucessful)
- */
+*main -Entry point 
+*Return: 0 (Sucessful)
+*/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle\n\0");
 return (0);
 }
