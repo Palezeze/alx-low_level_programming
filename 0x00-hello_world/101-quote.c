216a5a02bb3fba823a9447c11d85d5fce0e115e9
Palezeze <palesashole@yahoo.com>
2023-06-19 13:34:53 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
+#include<unistd.h>
 /**
  * main -prints the phrase 
  *
- * Return: 1 (if successful)
+ * Return: Always 1 
  */
 int main(void)
 {
-	printf("and that piece of art is useful" (- Dora Korpar,\'n)2015-10-19");
-	return(1);
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
