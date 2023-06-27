951d8044a0f5be6ac0aad3f0e09c4a0640f63453
Palezeze <palesashole@yahoo.com>
2023-06-27 14:27:00 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -17,10 +17,10 @@ void puts2(char *str)
 		y++;
 			longi++;
 	}
-	t = longi -1;
+	t = longi - 1;
 	for (o = 0; o <= t; o++)
 	{
-		if (o % 2 ==0)
+		if (o % 2 == 0)
 		{
 			_putchar(str[o]);
 		}
