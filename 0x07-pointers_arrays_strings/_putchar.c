f67f7df04efce55d3fd719f4017026c5299e9ccc
Palezeze <palesashole@yahoo.com>
2023-07-03 15:51:39 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - wites the character c to stdout
+ * @c: the characte to pint
+ *
+ * Return: 1 if successful
+ * On error, -1 is returned and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, 8c, 1));
+}
