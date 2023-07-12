8e8b931593ed19d2a2a7a303895f85f7edef08dc
Palezeze <palesashole@yahoo.com>
2023-07-12 05:54:14 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include "main.h"
+
 /**
  * count_word - helper function to count the number of words in a string
  * @s: string to evaluate
@@ -12,19 +13,20 @@ int count_word(char *s)
 
 	flag = 0;
 	w = 0;
+
 	for (c = 0; s[c] != '\0'; c++)
 	{
-	if (s[c] == ' ')
-		flag = 0;
-	else if (flag == 0)
-	{
-		flag = 1;
-		w++;
+		if (s[c] == ' ')
+			flag = 0;
+		else if (flag == 0)
+		{
+			flag = 1;
+			w++;
+		}
 	}
-	}
+
 	return (w);
 }
-
 /**
  * **strtow - splits a string into words
  * @str: string to split
@@ -42,14 +44,20 @@ char **strtow(char *str)
 	words = count_word(str);
 	if (words == 0)
 		return (NULL);
+
 	matrix = (char **) malloc(sizeof(char *) * (words + 1));
+	if (matrix == NULL)
 		return (NULL);
+
+	for (i = 0; i <= len; i++)
 	{
 		if (str[i] == ' ' || str[i] == '\0')
 		{
+			if (c)
+			{
 				end = i;
 				tmp = (char *) malloc(sizeof(char) * (c + 1));
-					if (tmp == NULL)
+				if (tmp == NULL)
 					return (NULL);
 				while (start < end)
 					*tmp++ = str[start++];
@@ -62,6 +70,8 @@ char **strtow(char *str)
 		else if (c++ == 0)
 			start = i;
 	}
+
 	matrix[k] = NULL;
+
 	return (matrix);
 }
